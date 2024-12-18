1d443e6e191980bc167c18603c0e0cd82dc37c9b
Armond Vickie <armondvi@un-a2.kzn.21-school.ru>
2024-12-18 19:42:49 +0300

    17.12

--- t7.cc
+++ t7.cc
@@ -4,99 +4,101 @@ using namespace std;
 
 int main(){
 
+    //ДАМЫ И ГОСПОДА ЭТО ЛЮТЕЙШИЙ ГОВНОКОД НО ВРОДЕ РАБОЧИЙ
+
     const int n = 5;
     int a[n] = {0};
     for (int i = 0; i < n; i++){
         cin >> a[i];
     }
 
-    // //1
-    // int i = 0, x;
-    // cin >> x;
-    // for(; (i < n)&&(a[i] < x); i++){
-    // }
-    // if (a[i] == x)  cout << i << endl;
-    // else cout << "NO" << endl;
+    //1
+    int i = 0, x;
+    cin >> x;
+    for(; (i < n)&&(a[i] < x); i++){
+    }
+    if (a[i] == x)  cout << i << endl;
+    else cout << "NO" << endl;
 
-    // //2
-    // int l = 0, r = n-1, x;
-    // cin >> x;
-    // int flag = 1, mid;
-    // while (flag && (l<=r)){
-    //     mid = (l + r) / 2;
-    //     if (a[mid] > x)
-    //         r = mid-1;
-    //     else if (a[mid] < x)
-    //         l = mid+1;
-    //     else
-    //         flag = 0;
-    // }
-    // if (a[mid] == x)  cout << mid << endl;
-    // else cout << "NO" << endl;
+    //2
+    int l = 0, r = n-1, x;
+    cin >> x;
+    int flag = 1, mid;
+    while (flag && (l<=r)){
+        mid = (l + r) / 2;
+        if (a[mid] > x)
+            r = mid-1;
+        else if (a[mid] < x)
+            l = mid+1;
+        else
+            flag = 0;
+    }
+    if (a[mid] == x)  cout << mid << endl;
+    else cout << "NO" << endl;
 
 
-    // //3
-    // const int m = 6;
-    // int b[m] = {0}, c[m+n] = {0};
-    // for (int i = 0; i < m; i++){
-    //     cin >> b[i];
-    // }
-    // int ia = 0, ib = 0, ic = 0;
-    // for (; ib < m; ib++){
-    //     while ((ia < n) && (a[ia] <= b[ib])){
-    //         if ((ic == 0) || ((ic != 0) && (a[ia] != c[ic-1]))){
-    //             c[ic] = a[ia];
-    //             ic++;
-    //         }
-    //                         ia++;
-    //     }
-    //     if ((ic == 0) || ((ic != 0) && (b[ib] != c[ic-1]))){
-    //         c[ic] = b[ib];
-    //         ic++;
-    //     }
-    // }
-    // for(int i=0;i<ic;i++)
-    //     cout<<c[i]<<' ';
+    //3
+    const int m = 6;
+    int b[m] = {0}, c[m+n] = {0};
+    for (int i = 0; i < m; i++){
+        cin >> b[i];
+    }
+    int ia = 0, ib = 0, ic = 0;
+    for (; ib < m; ib++){
+        while ((ia < n) && (a[ia] <= b[ib])){
+            if ((ic == 0) || ((ic != 0) && (a[ia] != c[ic-1]))){
+                c[ic] = a[ia];
+                ic++;
+            }
+                            ia++;
+        }
+        if ((ic == 0) || ((ic != 0) && (b[ib] != c[ic-1]))){
+            c[ic] = b[ib];
+            ic++;
+        }
+    }
+    for(int i=0;i<ic;i++)
+        cout<<c[i]<<' ';
 
-    // //4
-    // const int m = 6;
-    // int b[m] = {0}, c[m+n] = {0};
-    // for (int i = 0; i < m; i++){
-    //     cin >> b[i];
-    // }
-    // int ia = 0, ib = 0, ic = 0;
-    // for (; ib < m; ib++){
-    //     while ((ia < n) && (a[ia] < b[ib])){
-    //                         ia++;
-    //     }
-    //     if ((a[ia] == b[ib]) && ((ic == 0) || ((ic != 0) && (a[ia] != c[ic-1])))){
-    //             c[ic] = a[ia];
-    //             ic++;
-    //         }
-    // }
-    // for(int i=0;i<ic;i++)
-    //     cout<<c[i]<<' ';
+    //4
+    const int m = 6;
+    int b[m] = {0}, c[m+n] = {0};
+    for (int i = 0; i < m; i++){
+        cin >> b[i];
+    }
+    int ia = 0, ib = 0, ic = 0;
+    for (; ib < m; ib++){
+        while ((ia < n) && (a[ia] < b[ib])){
+                            ia++;
+        }
+        if ((a[ia] == b[ib]) && ((ic == 0) || ((ic != 0) && (a[ia] != c[ic-1])))){
+                c[ic] = a[ia];
+                ic++;
+            }
+    }
+    for(int i=0;i<ic;i++)
+        cout<<c[i]<<' ';
 
-    // //5
-    // const int m = 6;
-    // int b[m] = {0}, c[m+n] = {0};
-    // for (int i = 0; i < m; i++){
-    //     cin >> b[i];
-    // }
-    // int ia = 0, ib = 0, ic = 0;
-    // for (; ib < m; ib++){
-    //     while ((ia < n) && (a[ia] < b[ib])){
-    //         ia++;
-    //     }
-    //     if ((a[ia] != b[ib])&& ((ic == 0) || ((ic != 0) && (b[ib] != c[ic-1])))){
-    //         c[ic] = b[ib];
-    //         ic++;
-    //     }
-    // }
-    // for(int i=0;i<ic;i++)
-    //     cout<<c[i]<<' ';
+    //5
+    const int m = 6;
+    int b[m] = {0}, c[m+n] = {0};
+    for (int i = 0; i < m; i++){
+        cin >> b[i];
+    }
+    int ia = 0, ib = 0, ic = 0;
+    for (; ib < m; ib++){
+        while ((ia < n) && (a[ia] < b[ib])){
+            ia++;
+        }
+        if ((a[ia] != b[ib])&& ((ic == 0) || ((ic != 0) && (b[ib] != c[ic-1])))){
+            c[ic] = b[ib];
+            ic++;
+        }
+    }
+    for(int i=0;i<ic;i++)
+        cout<<c[i]<<' ';
 
-    //6
+    // 6
     const int m = 6;
     int b[m] = {0}, c[m+n] = {0};
     for (int i = 0; i < m; i++){
@@ -123,6 +125,45 @@ int main(){
     }
     for(int i=0;i<ic;i++)
         cout<<c[i]<<' ';
+
+
+    //7
+    int x;
+    cin >> x;
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        if (a[left] < x) {
+            left++;
+        } else if (a[right] >= x) {
+            right--;
+        } else {
+            swap(a[left], a[right]);//хз можно ли использовать но мне лень писать 3 строки свапа
+            left++;
+            right--;
+        }
+    }
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<' ';
+
+    //8
+    ////полагаю что массив упорядочен, иначе анлак, меньше o(n) вряд-ли выйдет
+    int l = 0, r = n-1;
+    int flag = 1, mid;
+    while (flag && (l<=r)){
+        mid = (l + r) / 2;
+        if (a[mid] == 1)
+            r = mid-1;
+        else if (a[mid] == 0)
+            l = mid+1;
+    }
+      cout << mid - 1 << " " << mid << endl;
+
+
+
+    // 9 со звездочкой написано, значит не обяз
 
 
 
