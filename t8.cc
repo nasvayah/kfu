7e2a835c2733b824d7882d4e5078f4289225cafd
varvara <varvaraparamon@gmail.com>
2024-12-19 17:29:41 +0300

    19.12

---
+++ t8.cc
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    // /////////////////////////38 
+    // const int n = 2;
+    // int a[n][n];
+    
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         cin >> a[i][j];
+    // }
+    // double res = 0;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++) //врчную заранее задали что м = н
+    //         res += a[i][j] * a[i][j];
+    // }
+
+    // cout << pow(res, 0.5) << endl;
+
+    // ///////////////////////////////////39
+    // const int n = 3;
+    // int a[n][n] = {{0}};
+
+    // // //б
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = 0; j <= i; j++)
+    // //         a[i][j] = j+1;
+    // // }
+
+    // // //в
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = i; j < n; j++)
+    // //         a[i][j] = j-i+1;
+    // // }
+    
+    // // //г
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = 0; j < n; j++)
+    // //         a[i][j] = i + j+2;
+    // // }
+
+    // // //д
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = 0; j < n; j++)
+    // //         a[i][j] = n - i - j;
+    // // }
+
+
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         cout << a[i][j] << " ";
+    //     cout << endl;
+    // }
+    
+    // ///////////////////////////////44
+    // const int n = 3;
+    // int a[n][n];
+    
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         cin >> a[j][i];
+    // }
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         cout << a[i][j] << " ";
+    //     cout << endl;
+    // }
+
+    // ////////////////////////////////41
+    // const int n = 3;
+    // int a[n][n], vec[n];
+
+    // for (int i = 0; i < n; i++){
+    //     cin >> vec[i];
+    // }
+
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         a[i][j] = vec[(j+i)%n];
+    // }
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++)
+    //         cout << a[i][j] << " ";
+    //     cout << endl;
+    // }
+
+
+    // /////////////////////////////47
+    // const int n = 4;
+    // int a[n][n] = {{0}}, vec[(n+1)*n/2];
+
+    // // //a
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = 0; j < n; j++)
+    // //         cin >> a[i][j];
+    // // }
+    // // for (int i = 0, vec_i = 0; i < n; i++){
+    // //     for (int j = 0; j <= i; j++){
+    // //         vec[vec_i++] = a[i][j];
+    // //     }
+    // // }
+    // // for (int i = 0; i < (n+1)*n/2; i++)
+    // //     cout << vec[i] << " ";
+    // // cout << endl;
+
+    // // //b
+    // // for (int i = 0; i < (n+1)*n/2; i++)
+    // //     cin >> vec[i];
+    // // for (int i = 0, vec_i = 0; i < n; i++){
+    // //     for (int j = 0; j <= i; j++)
+    // //         a[i][j] = vec[vec_i++];
+    // // }
+    // // for (int i = 0; i < n; i++){
+    // //     for (int j = 0; j < n; j++){
+    // //         cout <<  a[i][j] << " ";
+    // //     }
+    // //     cout << endl;
+    // // }
+
+
+    
+
+
+
+
+    return 0;
+}
